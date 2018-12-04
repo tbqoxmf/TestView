bfb7f6ccec85d00d5c1353d415c9d3269142911c
tbqoxmf <tbqoxmf@gmail.com>
2018-12-04 23:54:54 +0900

    test

---
+++ ArenaBattle/ABPawn.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ArenaBattle.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "ABPawn.generated.h"
+
+UCLASS()
+class ARENABATTLE_API AABPawn : public APawn
+{
+	GENERATED_BODY()
+	
+private:
+	void UpDown(float NewAxisValue);
+	void LeftRight(float NewAxisValue);
+
+public:
+	// Sets default values for this pawn's properties
+	AABPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void PostInitializeComponents() override;
+	virtual void PossessedBy(AController* NewController) override;
+
+
+	UPROPERTY(VisibleAnyWhere, Category=Collision)
+	UCapsuleComponent* Capsule;
+	
+	UPROPERTY(VisibleAnyWhere, Category = Visual)
+	USkeletalMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnyWhere, Category = Movement)
+	UFloatingPawnMovement* Movement;
+
+	UPROPERTY(VisibleAnyWhere, Category = Camera)
+	USpringArmComponent* SpringArm;
+	
+	UPROPERTY(VisibleAnyWhere, Category = Camera)
+	UCameraComponent* Camera;
+	
+};
